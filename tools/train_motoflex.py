74590537bc859728a9a54e3269ea82b848a4d142
KianRK <kian.rajabi.khamneh@iml.fraunhofer.de>
2024-08-22 01:29:26 +0200

    minor fix

--- tools/train_motoflex.py
+++ tools/train_motoflex.py
@@ -45,7 +45,7 @@ rew_terms = [
     lambda _, __, ___, periodic_reward_values: np.sum(WalkingSimulator.foot_contact(1) * periodic_reward_values["expected_c_frc_right"]),
     lambda _, __, ___, periodic_reward_values: np.sum(periodic_reward_values["expected_c_spd_right"] * norm(WalkingSimulator.get_right_foot_velocity())),
     lambda _, obs, __, ___: - 1 * np.sum(np.abs(20*(obs['target_forwards_vel'][0]-obs['current_lin_vel'][0]))),
-    lambda env, obs, _, __: -1 * env.compute_quaternion_difference(obs["current_body_orientation_quaternion"]),
+    lambda env, obs, _, __: -1 * np.sum(env.compute_quaternion_difference(obs["current_body_orientation_quaternion"])),
     lambda _, __, last_action, ___: -0.01 * np.sum(np.abs(last_action)),
     lambda _, obs, __, ___: -1 * np.abs(obs["current_lin_vel"][1]),
     lambda _, obs, __, ___: -0.01 * np.sum(np.abs(obs["current_joint_torques"])),
